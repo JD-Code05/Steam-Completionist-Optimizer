e54e544bf87cc36de30caafd73cc9b84cc606ee1
John Daniel L. Ubungen <jdubungen805@gmail.com>
2026-01-17 15:33:20 +0800

    Progress Code (script.js) 4.2 - Finished Update Data Code of the Website

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 let knownAchievements = new Set();
 let firstRun = true;
 
-async function updateData(){
+async function updateData() {
     try {
         const response = await fetch('/api.data');
         const data = await response.json();
@@ -19,6 +19,29 @@ async function updateData(){
                 card.classList.add('recommended');
                 card.innerHTML += `<div style="color:#a4d007; font-weight:bold; font-size:0.8em">RECOMMENDED NEXT</div>`;
             }
-        }
+            card.innerHTML += `
+                <h3>${ach.name}</h3>
+                <p>${ach.description}</p>
+                <small>${ach.rarity.toFixed(1)}% Global Unlock</small>
+            `;
+            list.appendChild(card);
+
+            if (ach.achieved) completedCount++;
+
+            if (!firstRun && ach.achieved && !knownAchievements.has(ach.apiname)) {
+                showToast(ach.name, ach.description);
+            }
+            if (ach.achieved) knownAchievements.add(ach.apiname);
+        });
+
+        const percent = ((completedCount / data.length) * 100).toFixed(1);
+        const bar = document.getElementById('progressBar');
+        bar.style.width = percent + '%';
+        bar.innerText = percent + '%''
+
+        firstRun = false;
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
     }
 }
